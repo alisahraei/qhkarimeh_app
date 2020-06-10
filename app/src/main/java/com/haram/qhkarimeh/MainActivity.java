e31e83971101e9f9a6f299fe4c78dac88930b93b
Ali Sahraei <ali.sahraei110@gmail.com>
2020-06-10 23:35:14 +0430

    build webview

--- app/src/main/java/com/haram/qhkarimeh/MainActivity.java
+++ app/src/main/java/com/haram/qhkarimeh/MainActivity.java
@@ -2,13 +2,69 @@ package com.haram.qhkarimeh;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    WebView webView;
+    String url = "https://qhkarimeh.ir/";
+    boolean doubleBackToExitPressedOnce = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webview);
+        webView.loadUrl(url);
+        webView.setWebViewClient(new WebViewClient());
+        websetting();
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+    @SuppressLint("JavascriptInterface")
+    public void websetting(){
+        WebSettings webSetting = webView.getSettings();
+        webSetting.setJavaScriptEnabled(true);
+        webSetting.setDomStorageEnabled(true);
+        webSetting.setPluginState(WebSettings.PluginState.ON);
+        webSetting.setLoadsImagesAutomatically(true);
+        webSetting.setLoadWithOverviewMode(true);
+        webSetting.setRenderPriority(WebSettings.RenderPriority.HIGH);
+        webSetting.setSupportZoom(false);
+        webSetting.setSavePassword(false);
+        webSetting.setBlockNetworkImage(false);
+        webSetting.setSupportMultipleWindows(false);
+        webSetting.setAppCacheEnabled(true);
+        webSetting.setSupportZoom(false);
+        webSetting.setAllowFileAccess(true);
+        webSetting.setAllowContentAccess(true);
+        webSetting.setBuiltInZoomControls(true);
+        webSetting.setPluginState(WebSettings.PluginState.ON);
+        webView.setScrollbarFadingEnabled(false);
+        webView.setHorizontalScrollBarEnabled(false);
+        webView.setVerticalScrollBarEnabled(false);
+        webView.setScrollContainer(false);
+        webView.addJavascriptInterface(this, "jsinterface");
+        webView.setOverScrollMode(View.OVER_SCROLL_NEVER);
+        webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+        webView.setScrollbarFadingEnabled(false);
+    }
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            if (doubleBackToExitPressedOnce) {
+                super.onBackPressed();
+                return;
+            }
+            this.doubleBackToExitPressedOnce = true;
+            Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+        }
     }
 }
