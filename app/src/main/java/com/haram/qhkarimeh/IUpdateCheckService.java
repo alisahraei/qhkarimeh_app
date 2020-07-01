6594a96093ec5ba3bb51841209df605a02d11ce7
ali sahraei <ali.sahraei110@gmail.com>
2020-07-01 12:44:53 +0430

    test for checkupdate cafe bazar

---
+++ app/src/main/java/com/haram/qhkarimeh/IUpdateCheckService.java
@@ -0,0 +1,11 @@
+package com.haram.qhkarimeh;
+
+import android.support.v4.app.INotificationSideChannel;
+
+public class IUpdateCheckService {
+    public static INotificationSideChannel.Stub Stub;
+
+    public long getVersionCode(String s) {
+        return 0;
+    }
+}
--- app/src/main/java/com/haram/qhkarimeh/MainActivity.java
+++ app/src/main/java/com/haram/qhkarimeh/MainActivity.java
@@ -3,7 +3,14 @@ package com.haram.qhkarimeh;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v4.app.INotificationSideChannel;
+import android.util.Log;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -12,14 +19,23 @@ import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import im.delight.android.webview.AdvancedWebView;
+
 public class MainActivity extends AppCompatActivity {
-    WebView webView;
+    private AdvancedWebView webView;
     LinearLayout linearLayout;
     String url = "https://qhkarimeh.ir/";
+
+    IUpdateCheckService service;
+    UpdateServiceConnection connection;
+    private static final String TAG = "UpdateCheck";
+
     boolean doubleBackToExitPressedOnce = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initService();
+
         setContentView(R.layout.activity_main);
         webView = findViewById(R.id.webview);
         linearLayout = findViewById(R.id.pgdata);
@@ -85,5 +101,50 @@ public class MainActivity extends AppCompatActivity {
             this.doubleBackToExitPressedOnce = true;
             Toast.makeText(this, R.string.backMsg, Toast.LENGTH_SHORT).show();
         }
+    }
+
+
+
+    class UpdateServiceConnection implements ServiceConnection {
+        public void onServiceConnected(ComponentName name, IBinder boundService) {
+            service = (IUpdateCheckService) INotificationSideChannel.Stub
+                    .asInterface((IBinder) boundService);
+            try {
+                long vCode = service.getVersionCode("com.haram.qhkarimeh");
+                Toast.makeText(MainActivity.this, "Version Code:" + vCode,
+                        Toast.LENGTH_LONG).show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            Log.d(TAG, "onServiceConnected(): Connected");
+        }
+
+        public void onServiceDisconnected(ComponentName name) {
+            service = null;
+            Log.d(TAG, "onServiceDisconnected(): Disconnected");
+        }
+    }
+
+    private void initService() {
+        Log.i(TAG, "initService()");
+        connection = new UpdateServiceConnection();
+        Intent i = new Intent(
+                "com.farsitel.bazaar.service.UpdateCheckService.BIND");
+        i.setPackage("com.farsitel.bazaar");
+        boolean ret = bindService(i, connection, Context.BIND_AUTO_CREATE);
+        Log.d(TAG, "initService() bound value: " + ret);
+    }
+
+    /** This is our function to un-binds this activity from our service. */
+    private void releaseService() {
+        unbindService(connection);
+        connection = null;
+        Log.d(TAG, "releaseService(): unbound.");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        releaseService();
     }
 }
