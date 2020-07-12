e1f374e22805b403dd874bbc447e53f4c6984ee2
ali sahraei <ali.sahraei110@gmail.com>
2020-07-12 17:11:44 +0430

    test for new fragment

--- app/src/main/java/com/haram/qhkarimeh/BottomSheetFragment.java
+++ app/src/main/java/com/haram/qhkarimeh/BottomSheetFragment.java
@@ -5,23 +5,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.Nullable;
-
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 public class BottomSheetFragment extends BottomSheetDialogFragment {
+    listenerBottomSheet mListener;
 
     public static BottomSheetFragment newInstance() {
         BottomSheetFragment fragment = new BottomSheetFragment();
         return fragment;
     }
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    listenerBottomSheet mListener;
 
     public void setListener(listenerBottomSheet listener) {
         mListener = listener;
@@ -32,14 +24,17 @@ public class BottomSheetFragment extends BottomSheetDialogFragment {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.activity_bottom_sheet, container, false);
-        view.findViewById(R.id.mammad).setOnClickListener(view1 -> mListener.refresh());
-        view.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+        view.findViewById(R.id.refreshbox).setOnClickListener(view1 -> mListener.refresh());
         return view;
-
     }
 
     public interface listenerBottomSheet{
