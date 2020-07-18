a23f9deefc0f76e20242fad773e51115088b0447
ali sahraei <ali.sahraei110@gmail.com>
2020-07-18 08:10:29 +0430

    fix warn

--- app/src/main/java/com/haram/qhkarimeh/MainActivity.java
+++ app/src/main/java/com/haram/qhkarimeh/MainActivity.java
@@ -77,7 +77,7 @@ public class MainActivity extends AppCompatActivity  implements AdvancedWebView.
                 linearLayout.setVisibility(View.GONE);
             }
         });
-    };
+    }
 
     public void showBottomSheet() {
         BottomSheetFragment bottomSheetDialog = BottomSheetFragment.newInstance();
